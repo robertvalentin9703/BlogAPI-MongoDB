391be80c351e0a63add9a8bd72acebde10cb132c
Robert Valentin <robert.carp@crystal-system.eu>
2021-10-07 12:31:42 +0300

    first commit

---
+++ src/main/java/eu/crystal/BlogApiWithMongoDB/BlogApiWithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package eu.crystal.BlogApiWithMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiWithMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiWithMongoDbApplication.class, args);
+	}
+
+}
